e9d3509b9b3cc4440536e62f79ff8cfa5e958d76
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2024-05-09 11:24:31 +1000

    feat: Version `2.0.0-SNAPSHOT-12`

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 /* required for maven publication */
 group = "org.orca.kotlass"
-version = "2.0.0-SNAPSHOT-11"
+version = "2.0.0-SNAPSHOT-12"
 
 /**
  * GitHub User ID for publishing to GitHub Packages.
